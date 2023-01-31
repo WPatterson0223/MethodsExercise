f3f7c3c53f106c2faffc19558c99a58db2f4df52
Will Patterson <wlp223@aol.com>
2023-01-31 16:09:07 -0500

    exercise one

---
+++ MethodsExercise/Methods.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using MethodsExercise;
+namespace MethodsExercise;
+
+internal class Methods
+{
+	
+	
+		public static void Story()
+		{
+			Console.WriteLine("Enter your ex's name.");
+			string exName = Console.ReadLine();
+			Console.WriteLine("Enter your least favorite color.");
+			string leastFavColor = Console.ReadLine();
+			Console.WriteLine("Enter somthing that smells bad.");
+			string badSmell = Console.ReadLine();
+			Console.WriteLine("Enter a number.");
+			string num = Console.ReadLine();
+			Console.WriteLine("Enter something that can explode.");
+			string bomb = Console.ReadLine();
+
+			Console.WriteLine($"There once was a monster named {exName}. It was a horrible {leastFavColor} creature \n" +
+				$"that smelled like {badSmell}. Which is why everybody was excited when it exploded into {num} peices after \n" +
+				$"it stepped onto a {bomb}");
+
+		}
+
+	
+}
+
--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -1,10 +1,10 @@
-﻿namespace MethodsExercise
+﻿using MethodsExercise;
+namespace MethodsExercise;
+
+public class Program
 {
-    public class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        Methods.Story();
     }
 }
